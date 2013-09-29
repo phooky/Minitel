86810e04c552cb93939b8d8c8851b5fba2906715
Adam Mayer <me@phooky.name>
2013-09-29 00:09:12 -0400

    added timeouts; default to 10 minutes without interaction.

--- information-terminal/run_process.py
+++ information-terminal/run_process.py
@@ -6,7 +6,19 @@ import pty
 import fcntl
 import time
 
-def make_run_process(command,wait_on_exit=False):
+class Timeout:
+    def __init__(self,duration=-1):
+        self.duration = duration
+        self.stamp = time.time()
+    def kick(self):
+        self.stamp = time.time()
+    def expired(self):
+        if self.duration == -1: 
+            return False # never expires
+        else:
+            return time.time() - self.stamp > self.duration
+
+def make_run_process(command,wait_on_exit=False,no_input_timeout=60):
     logging.debug('Creating process hook for {}'.format(command))
     def fn(m, parents):
         logging.debug('Running {}'.format(command))
@@ -21,26 +33,29 @@ def make_run_process(command,wait_on_exit=False):
         procout = os.fdopen(master)
         fl = fcntl.fcntl(master, fcntl.F_GETFL)
         fcntl.fcntl(master, fcntl.F_SETFL, fl | os.O_NONBLOCK)
-        logging.debug('Nonblocking mode on {}'.format(master))
-        while proc.poll() == None:
+        tout = Timeout(no_input_timeout)
+        while proc.poll() == None and not tout.expired():
             try:
-                try:
-                    out = procout.read()
-                    if len(out) > 0:
-                        m.send(out)
-                except IOError:
-                    pass
-                kp = m.recv(10)
-                if kp:
-                    proc.stdin.write(kp)
-            except:
-                logging.exception('oops')
+                out = procout.read()
+                if len(out) > 0:
+                    m.send(out)
+            except IOError:
+                pass
+            kp = m.recv(10)
+            if kp:
+                tout.kick()
+                proc.stdin.write(kp)
             time.sleep(0.05)
-        if wait_on_exit:
+        if wait_on_exit and not tout.expired():
             m.newline()
             m.setTextMode(minitel.BOLD)
             m.send('PRESS ANY KEY TO RETURN TO MENU')
             while not m.recv(1):
                 pass
+        if tout.expired():
+            logging.info('Process timed out.')
+        if proc.poll() == None:
+            logging.info('Killing process.')
+            proc.kill()
         logging.debug('{} done; return code {}'.format(command,proc.poll()))
     return fn
