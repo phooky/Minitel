c254a5bbffb7215b493b9a15e9be54fdc191d80a
Trammell Hudson <hudson@osresearch.net>
2013-10-04 00:26:00 +0000

    lf/cr translation

--- information-terminal/run_process.py
+++ information-terminal/run_process.py
@@ -18,7 +18,7 @@ class Timeout:
         else:
             return time.time() - self.stamp > self.duration
 
-def make_run_process(command,wait_on_exit=False,no_input_timeout=10*60,col80=True):
+def make_run_process(command,wait_on_exit=False,no_input_timeout=10*60,col80=True,lfcr=True):
     logging.debug('Creating process hook for {}'.format(command))
     def fn(m, parents):
         logging.debug('Running {}'.format(command))
@@ -28,10 +28,12 @@ def make_run_process(command,wait_on_exit=False,no_input_timeout=10*60,col80=Tru
         m.setTextMode(minitel.NORMAL)
         m.moveCursor(0,0)
         master, slave = pty.openpty()
+        env = os.environ
+        env['TERM']='minitel1b-80'
         proc = subprocess.Popen(command,
                                 stdin=subprocess.PIPE,
                                 stdout=slave,
-                                env={'TERM':'vt100'})
+                                env=env)
         procout = os.fdopen(master)
         fl = fcntl.fcntl(master, fcntl.F_GETFL)
         fcntl.fcntl(master, fcntl.F_SETFL, fl | os.O_NONBLOCK)
@@ -45,6 +47,8 @@ def make_run_process(command,wait_on_exit=False,no_input_timeout=10*60,col80=Tru
                 pass
             kp = m.recv(10)
             if kp:
+                if lfcr:
+                    kp = kp.replace('\r','\n')
                 tout.kick()
                 proc.stdin.write(kp)
             time.sleep(0.05)
